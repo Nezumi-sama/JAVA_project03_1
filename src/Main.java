c2eb6c70430a3b77377d4265a342a32551c3f117
Nezumi-sama <kryska007@yandex.ru>
2022-02-04 13:24:49 +0300

    First proj03_1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //объявлем переменные и вводим данные
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+
+    }
+}
